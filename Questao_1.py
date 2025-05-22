025ac1ac23b82d56f90305ed3935d065bf5e5938
João Gabriel <84649313+JoaoGabrielSF@users.noreply.github.com>
2025-05-22 19:28:53 -0300

    Update Questao_1.py

--- Questao_1.py
+++ Questao_1.py
@@ -3,8 +3,8 @@ taxa_euro = 6.15
 
 valor_reais = 100.00
 
-valor_em_euro = round(valor_reais * taxa_euro, 2)
-valor_em_dolar = round(valor_reais * taxa_dolar, 2)
+valor_em_euro = round(valor_reais / taxa_euro, 2)
+valor_em_dolar = round(valor_reais / taxa_dolar, 2)
 
 print(valor_em_euro," ",valor_em_dolar)
 
